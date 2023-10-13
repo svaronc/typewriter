304655da2c0a0f7bdfe310113e8fa16d2d353009
Sebastian varon <sebastianvaron96@gmail.com>
2023-10-13 13:02:40 +0000

    put the code into a function

--- typewriter.js
+++ typewriter.js
@@ -1,21 +1,14 @@
-const sentence = "hello there from lighthouse labs";
-const sentece2 = "we complete the assignment!!!!"
+const timer = function (sentence) {
+  let arrWords = sentence.split("");
+  let delay = 50;
 
-let arrWords = sentence.split("");
-let arrWords2 = sentece2.split("");
-let delay = 50;
+  for (const char of arrWords) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, delay);
+    delay += 50;
+  }
+  setTimeout(() => console.log(), delay);
+};
 
-for (const char of arrWords) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, delay);
-  delay += 50;
-}
-setTimeout(() => console.log(), delay);
-
-for (const chart of arrWords2) {
-  setTimeout(() => {
-    process.stdout.write(chart);
-  }, delay);
-  delay += 50;
-}
+timer("Hello my name is sebastian");
